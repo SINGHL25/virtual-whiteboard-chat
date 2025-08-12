63a3b5f1980a290dec069f844bf0694900cce061
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-12 20:45:16 +1000

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,8 @@
 
+// server/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ ok: true, service: 'virtual-whiteboard' }));
+
+module.exports = router;
